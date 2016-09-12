0231b7c6a817a8a43d606f97cc45de9ad6883418
Jason Rutherford <jason.rutherford@plansource.com>
2016-09-12 09:46:51 -0700

    Players list ui tweaks

--- app/helpers/nfl_players_helper.rb
+++ app/helpers/nfl_players_helper.rb
@@ -1,2 +1,14 @@
 module NflPlayersHelper
+
+  def ui_label_color(position)
+    case position
+      when "QB" then "violet"
+      when "WR" then "blue"
+      when "RB" then "green"
+      when "TE" then "teal"
+      when "DEF" then "red"
+      when "K" then "yellow"
+    else "orange"
+    end
+  end
 end
